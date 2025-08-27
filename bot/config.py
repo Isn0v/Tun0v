035c8aca3495adde83a21506c7bc7fe22cad610e
Isn0v <isnovikov0@gmail.com>
2025-08-27 12:22:43 +0700

    config: add download url template for song from YouTube

--- bot/config.py
+++ bot/config.py
@@ -1,4 +1,7 @@
 import os
+from ytmusicapi import YTMusic
+
+ytmusic = YTMusic()
 
 AUDIO_PATH = './downloads/audio'
 AUDIO_NAME = 'audio'
@@ -6,10 +9,13 @@ AUDIO_EXT = 'm4a'
 
 METADATA_PATH = './downloads/metadata'
 METADATA_NAME = 'media'
-METADATA_JSON_EXT = "info.json"
+METADATA_EXT = 'info.json'
 
 
 telegram_bot_token = os.environ.get('TELEGRAM_BOT_TOKEN', '')
 if telegram_bot_token == '':
   raise Exception('TELEGRAM_BOT_TOKEN is not set')
+
+
+SONG_URL_TEMPLATE = 'https://music.youtube.com/watch?v={}'
 
