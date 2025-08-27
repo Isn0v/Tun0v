026739e4339aa74f34818d513962f0c64ef52c79
Isn0v <isnovikov0@gmail.com>
2025-08-27 12:09:22 +0700

    scripts: tidy

--- scripts/download-audio.sh
+++ scripts/download-audio.sh
@@ -6,13 +6,4 @@ AUDIO_PATH=./downloads/audio
 AUDIO_NAME="audio"
 AUDIO_EXT="m4a"
 
-METADATA_PATH=./downloads/metadata
-METADATA_NAME="media"
-
-THUMBNAIL_EXT="jpg"
-
-yt-dlp --no-post-overwrites \
-  --quiet --no-warnings \
-  --paths "${AUDIO_PATH}" --output "${AUDIO_NAME}" \
-  --embed-thumbnail --embed-metadata --add-metadata \
-  -f "${AUDIO_EXT}/bestaudio/best" -o "${AUDIO_NAME}.${AUDIO_EXT}" "$1"
+yt-dlp --no-post-overwrites --quiet --no-warnings --paths "${AUDIO_PATH}" --output "${AUDIO_NAME}.${AUDIO_EXT}" --embed-thumbnail --embed-metadata --add-metadata -f "${AUDIO_EXT}/bestaudio/best" "$1"
--- scripts/get-audio-info.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-METADATA_PATH=./downloads/metadata/
-METADATA_NAME="media"
-
-yt-dlp --skip-download --quiet --no-warnings \
-  --write-info-json --paths "$METADATA_PATH" --output "$METADATA_NAME" "$1"
