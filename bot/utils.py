6e44f20a1bd453f8f30403b8547c216e1d18d276
Isn0v <isnovikov0@gmail.com>
2025-08-28 21:05:00 +0700

    utils: move load_audio and dump metadata from downloader.py

--- bot/utils.py
+++ bot/utils.py
@@ -1,8 +1,24 @@
-import validators
+import bot.config as config
+from bot.logger import logger
 
-def is_valid_url(url: str) -> bool:
-  try:
-    validators.url(url)
-    return True
-  except validators.ValidationError:
-    return False
+
+import json
+import os
+
+
+def dump_metadata(filename: str, metadata: dict) -> None:
+  metadata_path = f'{config.METADATA_PATH}/{filename}.{config.METADATA_EXT}'
+  logger.info(f'Dumping metadata into {os.path.abspath(metadata_path)}')
+  with open(metadata_path, 'w') as f:
+    json.dump(metadata, f, indent=2)
+
+  logger.info('Metadata dumped')
+
+
+def load_audio(filename: str) -> bytes:
+  (audio_path) = f'{config.AUDIO_PATH}/{filename}.{config.AUDIO_EXT}'
+  logger.info(f'Loading audio from {os.path.abspath((audio_path))}')
+  with open((audio_path), 'rb') as f:
+    return f.read()
+
+  logger.info('Audio loaded')
