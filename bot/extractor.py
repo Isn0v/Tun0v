26e8c2035c0906c0728d346cb15175e30db7ec8a
Isn0v <isnovikov0@gmail.com>
2025-08-28 13:52:16 +0700

    extractor: add playlist extraction logic with logging

--- bot/extractor.py
+++ bot/extractor.py
@@ -1,44 +1,143 @@
-import config
-import json
+
 from typing import Callable
 
+from logger import logger
+
+SONG_URL_TEMPLATE = 'https://music.youtube.com/watch?v={}'
+PLAYLIST_URL_TEMPLATE = 'https://music.youtube.com/playlist?list={}'
+
+# TODO: extract method must be async
+def extract(metadata: dict, filter: str) -> str:
+  logger.debug(f'Extracting metadata with filter {filter}')
+  extractor = filter_to_extractor[filter]
+  
+  return extractor(metadata)
+
+# region Song Metadata Extraction
 def extract_song(metadata: dict) -> str:
-  name = f'{metadata["artist"]} - {metadata["title"]} [{metadata["album"]["name"]}]'
-  link = f'{config.SONG_URL_TEMPLATE.format(metadata["videoId"])}'
-  return f'[{name}]({link})'
+  logger.debug(f'Extracting song metadata')
+  
+  album = get_song_album(metadata)
+  name = f'{get_song_title(metadata)} - {get_song_performers(metadata)} \\[{album}\\]'
+  if album == "":
+      name = f'{get_song_title(metadata)} - {get_song_performers(metadata)}'
 
+  link = format_song_url(get_song_video_id(metadata))
+  
+  extracted = f'[{name}]({link})\n'
+  logger.debug(f'Extracted song metadata: {extracted}')
+  return extracted
 
+def get_song_duration(metadata: dict) -> int:
+  logger.debug(f'Extracting song duration')
+  extracted = metadata['duration_seconds']
+  logger.debug(f'Extracted song duration: {extracted}')
+  return extracted
+
+def get_song_performers(metadata: dict) -> str:
+  logger.debug(f'Extracting song performers')
+  performers = ''
+  for artist in metadata['artists']:
+    performers += f'{artist["name"]}, '
+  performers = performers[:-2]
+  extracted = performers
+  logger.debug(f'Extracted song performers: {extracted}')
+  return extracted
+
+def get_song_title(metadata: dict) -> str:
+  logger.debug(f'Extracting song title')
+  extracted = metadata['title']
+  logger.debug(f'Extracted song title: {extracted}')
+  return extracted
+
+def get_song_thumbnail_url(metadata: dict) -> str:
+  logger.debug(f'Extracting song thumbnail')
+  extracted = metadata['thumbnails'][0]['url']
+  logger.debug(f'Extracted song thumbnail: {extracted}')
+  return extracted
+
+def get_song_album(metadata: dict) -> str:
+  logger.debug(f'Extracting song album')
+  if not metadata['album']:
+    logger.debug(f'No song album found')
+    return ''
+  
+  extracted = metadata['album']['name']
+  logger.debug(f'Extracted song album: {extracted}')
+  return extracted
+
+def get_song_video_id(metadata: dict) -> str:
+  logger.debug(f'Extracting song video id')
+  extracted = metadata['videoId']
+  logger.debug(f'Extracted song video id: {extracted}')
+  return extracted
+
+def format_song_url(video_id: str) -> str:
+  logger.debug(f'Formatting song url with video id {video_id}')
+  song_url = SONG_URL_TEMPLATE.format(video_id)
+  extracted = song_url
+  logger.debug(f'Formatted song url: {extracted}')
+  return extracted
+# endregion
+
+# region Playlist Metadata Extraction
 def extract_playlist(metadata: dict) -> str:
-  # TODO
-  raise NotImplementedError
+  logger.debug(f'Extracting playlist metadata')
+  name = get_playlist_title(metadata)
+  link = format_playlist_url(get_playlist_id(metadata))
+  
+  tracks = ''
+  for i, song in enumerate(metadata['tracks']):
+    tracks += f'{i + 1}. {extract_song(song)}'
+    
+  description = get_playlist_description(metadata)
+  if description:
+    description = 'Описание:\n' + \
+        f'{description}\n'
+  
+  extracted = f'[{name}]({link}):\n' + \
+        description + \
+        'Треки:\n' + \
+        f'{tracks}'
+  logger.debug(f'Extracted playlist metadata: {extracted}')
+  return extracted
+
+def get_playlist_id(metadata: dict) -> str:
+  logger.debug(f'Extracting playlist id')
+  playlist_id = metadata['id']
+  logger.debug(f'Extracted playlist id: {playlist_id}')
+  return playlist_id
+
+def get_playlist_title(metadata: dict) -> str:
+  logger.debug(f'Extracting playlist title')
+  title = metadata['title']
+  logger.debug(f'Extracted playlist title: {title}')
+  return title
+
+def get_playlist_description(metadata: dict) -> str:
+  logger.debug(f'Extracting playlist description')
+  description = metadata['description']
+  if not description:
+    logger.debug(f'No playlist description found')
+    description = ''
+  logger.debug(f'Extracted playlist description: {description}')
+  return description
+
+def get_playlist_duration(metadata: dict) -> str:
+  logger.debug(f'Extracting playlist duration')
+  duration = metadata['duration']
+  logger.debug(f'Extracted playlist duration: {duration}')
+  return duration
+
+def format_playlist_url(playlist_id: str) -> str:
+  logger.debug(f'Formatting playlist url with playlist id {playlist_id}')
+  playlist_url = PLAYLIST_URL_TEMPLATE.format(playlist_id)
+  logger.debug(f'Formatted playlist url: {playlist_url}')
+  return playlist_url
+# endregion
 
 
 filter_to_extractor : dict[str, Callable[[dict], str]] = {
   'songs': extract_song,
   'playlists': extract_playlist
 }
-
-def search_then_dump(query: str, filter: str) -> None:
-  songs = config.ytmusic.search(query, filter=filter, limit=1)
-
-  with open(f']{config.METADATA_PATH}/{config.METADATA_NAME}.{config.METADATA_EXT}', 'w') as f:
-    json.dump(songs, f, indent=2)
-    
-    
-def extract_after_dump(filter: str) -> str:
-  '''
-  Extracts information about songs which were prevously searched by search_then_dump method 
-  and returns list of hyperlinks in markdown format
-  '''
-  
-  extractor = filter_to_extractor[filter]
-  metadata = {}
-  
-  with open(f'{config.METADATA_PATH}/{config.METADATA_NAME}.{config.METADATA_EXT}', 'r') as f:
-    metadata = json.load(f)
-  
-  return extractor(metadata)
-  
-def extract_info(query: str, filter: str) -> str:
-  search_then_dump(query, filter)
-  return extract_after_dump(filter)
