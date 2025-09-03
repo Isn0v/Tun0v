c3fb5676b66986141e92934bea4a6153354917ff
Isn0v <isnovikov0@gmail.com>
2025-09-03 13:36:09 +0700

    config: refactor

--- bot/config.py
+++ bot/config.py
@@ -4,38 +4,39 @@ from ytmusicapi import YTMusic
 
 ytmusic = YTMusic()
 
-AUDIO_PATH = os.environ.get('AUDIO_PATH','./downloads/audio')
+# Directories
+AUDIO_PATH = os.environ.get('AUDIO_PATH', './downloads/audio')
 logger.debug(f"AUDIO_PATH: {AUDIO_PATH}")
-
-AUDIO_NAME = os.environ.get('AUDIO_NAME', 'audio')
-logger.debug(f"AUDIO_NAME: {AUDIO_NAME}")
-
-AUDIO_EXT = os.environ.get('AUDIO_EXT', 'm4a')
-logger.debug(f"AUDIO_EXT: {AUDIO_EXT}")
-
 METADATA_PATH = os.environ.get('METADATA_PATH', './downloads/metadata')
 logger.debug(f"METADATA_PATH: {METADATA_PATH}")
 
+# File names
+AUDIO_NAME = os.environ.get('AUDIO_NAME', 'audio')
+logger.debug(f"AUDIO_NAME: {AUDIO_NAME}")
 METADATA_NAME = os.environ.get('METADATA_NAME', 'media')
 logger.debug(f"METADATA_NAME: {METADATA_NAME}")
 
+# File extensions
+AUDIO_EXT = os.environ.get('AUDIO_EXT', 'm4a')
+logger.debug(f"AUDIO_EXT: {AUDIO_EXT}")
 METADATA_EXT = os.environ.get('METADATA_EXT', 'info.json')
 logger.debug(f"METADATA_EXT: {METADATA_EXT}")
 
-telegram_bot_token = os.environ.get('TELEGRAM_BOT_TOKEN', '')
-logger.debug(f"TELEGRAM_BOT_TOKEN: {telegram_bot_token}")
-if telegram_bot_token == '':
+# Telegram bot token
+telegram_bot_token = os.environ.get('TELEGRAM_BOT_TOKEN')
+if not telegram_bot_token:
   logger.error('TELEGRAM_BOT_TOKEN is not set')
   raise Exception('TELEGRAM_BOT_TOKEN is not set')
+logger.debug(f"TELEGRAM_BOT_TOKEN: {telegram_bot_token}")
 
-PYTHON_INTERPRETER = 'python3'
-
+# Scripts
+PYTHON_INTERPRETER = 'python'
 CLEANUP_SCRIPT_PATH = os.path.abspath('./bot/scripts/cleanup.py')
 logger.debug(f"CLEANUP_SCRIPT_PATH: {CLEANUP_SCRIPT_PATH}")
-
 DOWNLOADER_SCRIPT_PATH = os.path.abspath('./bot/scripts/downloader.py')
 logger.debug(f"DOWNLOADER_SCRIPT_PATH: {DOWNLOADER_SCRIPT_PATH}")
 
+# Other
 logger.debug(f"Current working directory is {os.getcwd()}")
 
 BOT_REQUEST_READ_TIMEOUT_DEFAULT = 10
