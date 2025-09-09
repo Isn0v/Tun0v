6c3eca2b94c3f012ee5e7bc62300c700770145b5
Isn0v <isnovikov0@gmail.com>
2025-09-09 10:44:25 +0700

    subproc: fix log dump filepath

--- bot/subproc.py
+++ bot/subproc.py
@@ -7,6 +7,10 @@ from bot.logger import logger, LOG_PATH
 STDOUT_REDIRECT = asyncio.subprocess.PIPE
 STDERR_REDIRECT = asyncio.subprocess.PIPE
 
+LOG_SUBPROCESSES = os.environ.get('LOG_SUBPROCESSES', 'false')
+logger.debug(f"LOG_SUBPROCESSES: {LOG_SUBPROCESSES}")
+
+
 # TODO: limit the number of subprocesses if needed
   
 async def invoke_async_subprocess(command: str, args: str) -> None:
@@ -22,16 +26,18 @@ async def invoke_async_subprocess(command: str, args: str) -> None:
   
   stdout, stderr = await process.communicate()
   
+  script_name = os.path.basename(args_array[0])
+  
   logger.debug(f'Command {command} {args} exited with code {process.returncode}')
-  if os.environ.get('LOG_SUBPROCESSES', 'false') == 'true':
-    with open(f'{LOG_PATH}/{args_array[0]}.log', 'a') as f:
+  if LOG_SUBPROCESSES == 'true':
+    with open(f'{LOG_PATH}/{script_name}.log', 'a') as f:
       logger.debug(f'Dumping subprocess stdout to {f.name}')
       f.write(stdout.decode('utf-8'))
   
   if process.returncode != 0:
     logger.error(f'Command {command} {args} failed')
-    if os.environ.get('LOG_SUBPROCESSES', 'false') == 'true':
-      with open(f'{LOG_PATH}/{args_array[0]}.log', 'a') as f:
+    if LOG_SUBPROCESSES == 'true':
+      with open(f'{LOG_PATH}/{script_name}.log', 'a') as f:
         logger.debug(f'Dumping subprocess stderr to {f.name}')
         f.write(stderr.decode('utf-8'))
     raise RuntimeError(f'Command {command} {args} failed')
